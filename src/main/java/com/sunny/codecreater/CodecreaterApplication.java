0842ebc0361717079090d76ccf948900410fd555
Zero656329 <865136678@qq.com>
2020-11-27 14:36:48 +0800

    代码生成器

---
+++ src/main/java/com/sunny/codecreater/CodecreaterApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.codecreater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecreaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecreaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunny/codecreater/CodecreaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.codecreater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecreaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
